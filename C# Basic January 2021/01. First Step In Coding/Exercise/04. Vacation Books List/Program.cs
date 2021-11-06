aefae9899a25c190672937f7f2136f2dffd10655
ZheniKoleva <zhenie_koleva@abv.bg>
2021-11-06 15:35:51 +0200

    update

---
+++ C# Basic January 2021/01. First Step In Coding/Exercise/04. Vacation Books List/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPages = int.Parse(Console.ReadLine());
+            double pagesPerHour = double.Parse(Console.ReadLine());
+            int daysToReadBook = int.Parse(Console.ReadLine());
+
+            double totalHoursToReadBook = numberOfPages / pagesPerHour;
+            double hoursPerDay = totalHoursToReadBook / daysToReadBook;
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
---
+++ C# Basic January 2021/01. First Step In Coding/Lab/01. Hello SoftUni/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
