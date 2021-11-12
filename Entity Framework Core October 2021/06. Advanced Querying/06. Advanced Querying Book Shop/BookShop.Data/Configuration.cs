e343449a9152141bb93a201babb8d2f79e95cb64
ZheniKoleva <zhenie_koleva@abv.bg>
2021-11-12 14:32:54 +0200

    update

--- Entity Framework Core October 2021/06. Advanced Querying/06. Advanced Querying Book Shop/BookShop.Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookShop.Data
-{
-    internal class Configuration
-    {
-        internal static string ConnectionString 
-            => "Server=.\\SQLEXpress;Database=BookShop;Integrated Security=True;";
-    }
-}
