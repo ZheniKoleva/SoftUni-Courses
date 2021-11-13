6652ecd272665de5f75372be141b3b7182795e96
ZheniKoleva <zhenie_koleva@abv.bg>
2021-11-13 16:40:02 +0200

    add new folder

---
+++ C# OOP October 2021/05. Exception Handling/04. Fixing Vol.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04.FixingVol2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            byte result;
+
+            firstNumber = 30;
+            secondNumber = 60;
+
+            try
+            {
+                result = Convert.ToByte(firstNumber * secondNumber);
+                Console.WriteLine($"{firstNumber} x {secondNumber} = {result}");
+            }
+            catch (OverflowException ofe)
+            {
+                Console.WriteLine(ofe.Message);
+            }           
+        }
+    }
+}
---
+++ C# OOP October 2021/05. Exception Handling/05. Convert To Double/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05.ConvertToDouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                double digit = Convert.ToDouble(input);
+                Console.WriteLine(digit);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+                throw fe;
+            }           
+            catch (OverflowException ofe)
+            {
+                Console.WriteLine(ofe.Message);
+                throw ofe;
+            }
+        }
+    }
+}
