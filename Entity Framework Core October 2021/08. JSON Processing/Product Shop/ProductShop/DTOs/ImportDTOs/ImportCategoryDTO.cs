2ee897ecc2eb0232c4be7516b07c9fd6503ac11a
ZheniKoleva <zhenie_koleva@abv.bg>
2021-11-16 02:08:50 +0200

    modify

---
+++ Entity Framework Core October 2021/08. JSON Processing/Product Shop/ProductShop/DTOs/ImportDTOs/ImportCategoryDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.DTOs.ImportDTOs
+{
+    public class ImportCategoryDTO
+    {
+        public string Name { get; set; }
+    }
+}
