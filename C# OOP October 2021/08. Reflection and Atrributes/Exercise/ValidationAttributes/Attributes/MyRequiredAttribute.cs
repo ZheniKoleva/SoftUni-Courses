f85391e8ba0b1855bc9cffecd6234a9a2229d820
ZheniKoleva <zhenie_koleva@abv.bg>
2021-11-18 13:14:51 +0200

    update

--- C# OOP October 2021/08. Reflection and Atrributes/Exercise/ValidationAttributes/Attributes/MyRequiredAttribute.cs
+++ C# OOP October 2021/08. Reflection and Atrributes/Exercise/ValidationAttributes/Attributes/MyRequiredAttribute.cs
@@ -3,14 +3,6 @@
     public class MyRequiredAttribute : MyValidationAttribute
     {
         public override bool IsValid(object obj)
-        {
-            if (obj == null)
-            {
-                return false;
-            }
-
-            return true;
-        }
-            
+	   => obj != null
     }
 }
