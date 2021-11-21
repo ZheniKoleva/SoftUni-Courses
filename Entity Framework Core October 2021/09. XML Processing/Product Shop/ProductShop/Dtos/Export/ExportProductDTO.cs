0f65056535e83d48bd2f81923d3135ef9537618a
ZheniKoleva <zhenie_koleva@abv.bg>
2021-11-21 17:41:00 +0200

    Add new solved tasks

---
+++ Entity Framework Core October 2021/09. XML Processing/Product Shop/ProductShop/Dtos/Export/ExportProductDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Export
+{
+    [XmlType("Product")]
+    public class ExportProductDTO
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ Entity Framework Core October 2021/09. XML Processing/Product Shop/ProductShop/Dtos/Export/ExportUserSoldProductsDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Export
+{
+    [XmlType("User")]
+    public  class ExportUserSoldProductsDTO
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlArray("soldProducts")]
+        public ExportProductDTO[] ProductsSold { get; set; }
+
+    }
+}
