dffcc87a764a7118f5f8b897893b00eca5a32b9e
ZheniKoleva <zhenie_koleva@abv.bg>
2021-11-27 23:58:06 +0200

    update

--- Entity Framework Core October 2021/13. Code First/Code-First/P03_SalesDatabase/Data/Connection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace P03_SalesDatabase.Data
-{
-    public static class Connection
-    {
-        public const string CONNECTION =
-            @"Server=.\SQLEXPRESS;Database=SalesDatabase;Integrated Security=True;";
-    }
-}
