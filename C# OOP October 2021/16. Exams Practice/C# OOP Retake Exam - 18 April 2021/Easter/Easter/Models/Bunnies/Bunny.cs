3c39f0c68cff997514ea827914e7e5b6aa7851a9
ZheniKoleva <zhenie_koleva@abv.bg>
2021-12-09 20:27:44 +0200

    add new exam

---
+++ C# OOP October 2021/16. Exams Practice/C# OOP Retake Exam - 18 April 2021/Easter/Easter/Models/Bunnies/Bunny.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Easter.Models.Bunnies.Contracts;
+using Easter.Models.Dyes.Contracts;
+using Easter.Utilities.Messages;
+
+namespace Easter.Models.Bunnies
+{
+    public abstract class Bunny : IBunny
+    {
+        private const int Default_Energy_Decrease = 10;
+
+        private string name;
+
+        private int energy;
+
+        private ICollection<IDye> dyes;
+
+        protected Bunny(string name, int energy)
+        {
+            Name = name;
+            Energy = energy;
+            dyes = new List<IDye>();
+        }
+
+        public string Name 
+        {
+            get => name;
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidBunnyName);
+                }
+
+                name = value;
+            } 
+        }
+
+        public int Energy
+        {
+            get => energy;
+            protected set
+            {
+                if (value < 0)
+                {
+                   value = 0;
+                }
+
+                energy = value;
+            }
+        }
+
+        public ICollection<IDye> Dyes => dyes;
+
+        public void AddDye(IDye dye)
+            => dyes.Add(dye);        
+
+        public virtual void Work()
+            => Energy -= Default_Energy_Decrease;
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            output.AppendLine($"Name: {Name}")
+                .AppendLine($"Energy: {Energy}")
+                .Append($"Dyes: {Dyes.Count(x => !x.IsFinished())} not finished");
+
+            return output.ToString();
+        }
+
+    }
+}
---
+++ C# OOP October 2021/16. Exams Practice/C# OOP Retake Exam - 18 April 2021/Easter/Easter/Models/Dyes/Dye.cs
@@ -0,0 +1,37 @@
+﻿using Easter.Models.Dyes.Contracts;
+
+namespace Easter.Models.Dyes
+{
+    public class Dye : IDye
+    {
+        private const int Default_Power_Decrease = 10;
+
+        private int power;
+
+        public Dye(int power)
+        {
+            Power = power;
+        }
+
+        public int Power
+        {
+            get => power;
+            private set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+
+                power = value;
+            }        
+        }
+
+
+        public bool IsFinished() => power == 0;
+        
+
+        public void Use()
+            => Power -= Default_Power_Decrease;       
+    }
+}
