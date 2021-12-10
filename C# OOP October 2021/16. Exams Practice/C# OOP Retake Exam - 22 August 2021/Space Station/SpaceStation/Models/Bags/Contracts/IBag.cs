5fffe3f97ba8a06d0fec5613de16903ca599d042
ZheniKoleva <zhenie_koleva@abv.bg>
2021-12-10 15:44:04 +0200

    add new exam

---
+++ C# OOP October 2021/16. Exams Practice/C# OOP Retake Exam - 22 August 2021/Space Station/SpaceStation/Models/Bags/Contracts/IBag.cs
@@ -0,0 +1,10 @@
+﻿namespace SpaceStation.Models.Bags.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface IBag
+    {
+        ICollection<string> Items { get; }
+        
+    }
+}
---
+++ C# OOP October 2021/16. Exams Practice/C# OOP Retake Exam - 22 August 2021/Space Station/SpaceStation/Repositories/PlanetRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+using SpaceStation.Repositories.Contracts;
+using SpaceStation.Models.Planets.Contracts;
+
+
+namespace SpaceStation.Repositories
+{
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private readonly IDictionary<string, IPlanet> models;
+
+        public PlanetRepository()
+        {
+            models = new Dictionary<string, IPlanet>();
+        }
+
+
+        public IReadOnlyCollection<IPlanet> Models
+            => (IReadOnlyCollection<IPlanet>)models.Values;
+
+
+        public void Add(IPlanet model)
+            => models.Add(model.Name, model);
+
+
+        public IPlanet FindByName(string name)
+            => models.ContainsKey(name) ? models[name] : null;
+
+
+        public bool Remove(IPlanet model)
+            => models.Remove(model.Name);
+
+    }
+}
