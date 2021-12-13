b4331f2cfb71f24634978641acb6c5c49c143386
ZheniKoleva <zhenie_koleva@abv.bg>
2021-12-13 21:57:44 +0200

    add solved oop exam

---
+++ C# OOP October 2021/17. Exam OOP 11 December 2021/01. Structure_Skeleton/Skeleton/Gym/Models/Gyms/WeightliftingGym.cs
@@ -0,0 +1,11 @@
+﻿namespace Gym.Models.Gyms
+{
+    public class WeightliftingGym : Gym
+    {
+        private const int Default_Capacity = 20;
+        public WeightliftingGym(string name) 
+            : base(name, Default_Capacity)
+        {
+        }
+    }
+}
