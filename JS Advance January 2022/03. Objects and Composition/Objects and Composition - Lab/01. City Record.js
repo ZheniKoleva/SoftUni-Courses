20a2b5fe00a20c8a525dfec925bcf18fbded1ec4
ZheniKoleva <zhenie_koleva@abv.bg>
2022-01-19 21:10:36 +0200

    add tasks

---
+++ JS Advance January 2022/03. Objects and Composition/Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,9 @@
+function createCity(name, population, treasury) {
+    const city = { name, population, treasury };
+
+    return city;
+}
+
+console.log(createCity('Tortuga', 7000, 15000));
+
+console.log(createCity('Santo Domingo', 12000, 23500));
---
+++ JS Advance January 2022/03. Objects and Composition/Objects and Composition - Lab/03. City Taxes.js
@@ -0,0 +1,28 @@
+function cityTaxes(name, population, treasury){
+    return {
+        name,
+        population,
+        treasury,
+        taxRate: 10, 
+        collectTaxes() {
+            this.treasury += Math.floor(this.population * this.taxRate);
+        },
+        applyGrowth(percentage){
+            this.population += Math.floor(this.population * (percentage / 100));
+        },
+        applyRecession(percentage){
+            this.treasury -= Math.ceil(this.treasury * (percentage / 100));
+        }
+    }  
+}
+
+
+const city = cityTaxes('Tortuga', 7000, 15000);
+console.log(city);
+
+
+const city = cityTaxes('Tortuga', 7000, 15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
