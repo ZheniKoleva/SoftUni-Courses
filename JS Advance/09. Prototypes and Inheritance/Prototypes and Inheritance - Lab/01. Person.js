760b5a27dbe0544edb96adf88eafa0475725ba0c
ZheniKoleva <zhenie_koleva@abv.bg>
2022-02-06 17:50:28 +0200

    add solved tasks

---
+++ JS Advance/09. Prototypes and Inheritance/Prototypes and Inheritance - Lab/01. Person.js
@@ -0,0 +1,41 @@
+function Person(firstName, lastName) {
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+    Object.defineProperty(this, 'fullName', {
+        set(value){
+            const [firstName, secondName] = value.split(' ');
+
+            if(value.split(' ').length == 2){
+                this.firstName = firstName;
+                this.lastName = secondName;
+            }
+        },
+
+        get(){
+            return `${this.firstName} ${this.lastName}`;
+        }
+    })
+}
+
+
+let person = new Person("Peter", "Ivanov");
+console.log(person.fullName); //Peter Ivanov
+person.firstName = "George";
+console.log(person.fullName); //George Ivanov
+person.lastName = "Peterson";
+console.log(person.fullName); //George Peterson
+person.fullName = "Nikola Tesla";
+console.log(person.firstName); //Nikola
+console.log(person.lastName); //Tesla
+
+console.log('=============================');
+
+let person2 = new Person("Albert", "Simpson");
+console.log(person2.fullName); //Albert Simpson
+person2.firstName = "Simon";
+console.log(person2.fullName); //Simon Simpson
+person2.fullName = "Peter";
+console.log(person2.firstName);  // Simon
+console.log(person2.lastName);  // Simpson
